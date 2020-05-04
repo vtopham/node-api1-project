147daf2098af5a7aaf4dbfbbddd95a59c9b6d8d1
Victoria  Topham <topham.victoria@gmail.com>
2020-05-04 15:26:54 -0700

    added cors for the stretch

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-
+const cors = require('cors')
 const server = express();
 
-
 server.use(express.json())
+server.use(cors())
+
 let users = [
     {
         id: 1,
