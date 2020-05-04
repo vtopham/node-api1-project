ef4552c766c7e589d756a846c306a0ef1a42993f
Victoria  Topham <topham.victoria@gmail.com>
2020-05-04 14:47:22 -0700

    followed first steps andhave first two parts of the post request working

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const server = express();
+
+server.use(express.json())
+const users = [
+    {
+        id: 1,
+        name: "Victoria",
+        bio: "My name is victoria"
+    }
+]
+
+
+
+server.get('/', (req, res) => {
+    res.send("It worked!")
+});
+
+//When a user wants to add a user
+server.post('/api/users', (req, res) => {
+    console.log(req.body)
+    //if the info is invalid or there is no body, respond with an error message
+    if (!req.body || !req.body.name || !req.body.id) {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user"})
+    } else { 
+    //if the information is valid
+    users.push({
+        id: req.body.id,
+        name: req.body.name,
+        bio: req.body.bio
+    })
+
+        res.status(201).json(users)
+    }
+
+})
+
+
+server.listen(8000, _ => console.log("listening on 8000"))
